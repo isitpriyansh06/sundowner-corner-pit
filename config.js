7182dc08f76bdc74a3fa6f3e7c9edc426b1ec008
isitpriyansh06 <choudhary.priyansh@gmail.com>
2026-07-13 16:43:01 +0530

    Update SHEET_ENDPOINT with the deployed URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.RAVES_CONFIG = Object.freeze({
   // Paste the deployed Google Apps Script web-app URL here.
-  SHEET_ENDPOINT: "",
+  SHEET_ENDPOINT: "https://script.google.com/macros/s/AKfycbyKdQIzl8x_40DdnghpbB97uyaW7k55I9W92p6aBZIOmfmqytb4b7itdxn6TZPKjR4/exec",
   // Private address revealed after RSVP.
   MAP_LINK: "https://maps.app.goo.gl/vJD5KaQXEj1DkYjy5",
   MAX_PLUS_ONES: 2
